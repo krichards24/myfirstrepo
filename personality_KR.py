85136a55c1113b4c38cc691c1c945188ef4e6b84
krichards24 <33523437+krichards24@users.noreply.github.com>
2017-11-09 11:30:20 -0500

    initial

---
+++ personality_KR.py
@@ -0,0 +1,19 @@
+name= "Queen K"
+state="New york"
+city= "Dubai/milan/paris/milan"
+book= " The selection series and A hundred horses"
+tvshow= "Kuwtk, Gossip girl, Riverdale, project runway"
+
+print (name + "likes to watch " + tvshow)
+
+videogame = "Just Dance"
+
+
+print ("What is your favorite subject?")
+subject= input ()
+
+if subject == "recess":
+    print ("That's my favorite too!")
+else:
+    print(subject + " is Ok too. ")
+print (" What's your favorite sport?
